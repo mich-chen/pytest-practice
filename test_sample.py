80faec68c762e78314783bf9232efe3aebfbbd1e
Michelle Chen <mich.chen.94@gmail.com>
2020-12-03 16:02:38 -0800

    pytest.raises practice

--- test_sample.py
+++ test_sample.py
@@ -2,14 +2,31 @@ import random
 import pytest
 # from pytest_mock import mocker
 
+# *************** Pytest Assertions ***************
 def func(x):
     return x + 1
 
 def test_answer():
     assert func(3) == 4
 
-def test_fail():
-    assert pytest.fail('testing pytest.fail', True)
+# def test_fail():
+    # assert pytest.fail('testing pytest.fail', True)
+
+# *************** Pytest.raises: expected exceptions and errors ***************
+
+def test_raise_pass():
+    """Pass == correctly raises exception/error."""
+
+    with pytest.raises(ZeroDivisionError):
+        1/0 # pass
+
+def test_raise_fail():
+    """Fail == expression does not raise exception/error."""
+
+    with pytest.raises(ZeroDivisionError):
+        1/1 # fail
+
+# *************** Pytest Mocker Spy ***************
 
 # def test_shuffle_spy():
 #     spy = mocker.spy(random, 'shuffle')
